4ac6b68efd788989ca0172289475bfc9aafb3c74
n_matsuo <n.matsuo1616@gmail.com>
2021-12-24 17:51:12 +0900

    first commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  text: string
+}
+
+export default (props: Props) => (
+  <h1>{props.text}</h1>
+)
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="tsx">
+import Hello from "../components/Hello"
+
+export default defineComponent({
+  render() {
+    return (
+      <div>
+        <Hello text='Hello Word!'/>
+      </div>
+    )
+  }
+})
+</script>
