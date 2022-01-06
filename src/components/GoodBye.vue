e8a63630ba565c4c2fda80ad8bcbcd3e046e9ea5
n_matsuo <n.matsuo1616@gmail.com>
2022-01-06 19:00:38 +0900

    goodbyeコンポーネント追加

---
+++ src/components/GoodBye.vue
@@ -0,0 +1,20 @@
+<script setup lang="tsx">
+type props = {
+  text: string
+}
+
+const { text } = defineProps<props>()
+</script>
+
+<script lang="tsx">
+export default defineComponent({
+  setup() {
+    return { text }
+  },
+  render() {
+    return (
+      <h1>GoodBye { this.text } !</h1>
+    )
+  }
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="tsx">
 import Hello from "../components/Hello"
 import Hello2 from "../components/Hello2"
+import GoodBye from "../components/GoodBye.vue"
 
 export default defineComponent({
   render() {
@@ -8,6 +9,7 @@ export default defineComponent({
       <div>
         <Hello text="Hello World!"/>
         <Hello2 text="Hello World!"/>
+        <GoodBye text='everyone'/>
       </div>
     )
   }
